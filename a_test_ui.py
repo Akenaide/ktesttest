34182f3c7b05da351e9649f7efeb5ea66f375684
Kevin ManSang <kevin.mansang@mhcomm.fr>
2013-10-11 16:50:48 +0200

    First test on a2.addi()

--- a_test_ui.py
+++ a_test_ui.py
@@ -1,1 +1,18 @@
-def
+import nose
+import sys
+import unittest
+from app_test import app_test
+from PyQt4.QtGui import QApplication
+from PyQt4.QtTest import QTest
+from PyQt4.QtCore import Qt
+
+class App_test_test(unittest.TestCase):
+    def setUp(self):
+        self.app = QApplication(sys.argv)
+        self.app_test = app_test()
+        
+    def test_addition(self):
+        QTest.keyClicks(self.app_test.ui.lEX, "3")
+        QTest.keyClicks(self.app_test.ui.lEY, "3")
+        QTest.mouseClick(self.app_test.ui.pButtonAdd, Qt.LeftButton)
+        self.assertEqual(self.app_test.ui.label_3.text() ,"6")
