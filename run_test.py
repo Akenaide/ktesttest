590120b1e1e3a3293fb13630610fa4fc8fd903bb
Kevin Man Sang <kevin.mansang@mhcomm.fr>
2013-11-26 17:13:17 +0100

    starting new way to run nose

--- run_test.py
+++ run_test.py
@@ -1,16 +1,18 @@
-import nose 
 
+from nose.loader import TestLoader 
+import os.path as path
+
+TOP_DIR = path.realpath(path.dirname(__file__))
 def main():
     import argparse
     import sys
-
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--nose", action="store_true",
             help="run nose")
     parser.add_argument("-u", "--unittest", action="store_true",
             help="run unittest",
-            const="-v")
+            )
 
     args = parser.parse_args()
     
@@ -18,13 +20,16 @@ def main():
 
         if args.nose is not False :
              
-            #args=None
-            backup, sys.argv = sys.argv, sys.argv[:1] + ["-v"]
-            nose.run()
-            sys.argv = backup
-    
+            run_nose() 
         else :
             print args.unittest
+
+def run_nose():
+    print "hey"
+    testloader = TestLoader()
+    for test in testloader.loadTestsFromDir(TOP_DIR):
+        test.runTest()
+        dir(test)
 
 if __name__ == '__main__':
     main()
